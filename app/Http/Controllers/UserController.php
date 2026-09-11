4c4cd7c4b3b7d1e9d6432b3e9f32412f0009ba9f
nazario22812 <76971123+nazario22812@users.noreply.github.com>
2026-09-11 21:52:46 +0200

    +prawiegotowybackdoplanu

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Grupastudenta;
+use App\Models\Planzajec;
+use App\Models\Rezerwacja;
 use App\Models\Student;
+use App\Models\Zajecie;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -11,7 +14,51 @@ class UserController extends Controller
 {
     //
     public function planzajec(Request $request){
-        return Inertia::render('PlanZajec');
+
+    
+    // SELECT
+    //     z.DzienTygodnia,
+    //     r.GodzinaRozpoczecia,
+    //     r.GodzinaZakonczenia,
+    //     p.nazwa AS Przedmiot,
+    //     z.TypZajec,
+    //     s.numerSali,
+    //     pro.idProwadzacy
+    // FROM zajęcie z
+    // JOIN przedmiot p
+    //     ON z.Przedmiot_idPrzedmiot = p.idPrzedmiot
+    // JOIN sala s
+    //     ON z.Sala_idSala = s.idSala
+    // JOIN prowadzacy pro
+    //     ON pro.Zajęcie_idZajęcie = z.idZajęcie
+    //     AND pro.Zajęcie_PlanZajec_idPlanZajec = z.PlanZajec_idPlanZajec
+    // JOIN rezerwacja r
+    //     ON z.idZajęcie = r.Zajęcie_idZajęcie
+    //     AND z.PlanZajec_idPlanZajec = r.Zajęcie_PlanZajec_idPlanZajec
+    // WHERE z.PlanZajec_idPlanZajec = 1
+    // ORDER BY
+    //     FIELD(
+    //         z.DzienTygodnia,
+    //         'Poniedziałek',
+    //         'Wtorek',
+    //         'Środa',
+    //         'Czwartek',
+    //         'Piątek'
+    //     ),
+    //     r.GodzinaRozpoczecia
+
+        $user = $request->user();
+        $student = Student::with('grupastudenta')->where('User_idUser', $user->idUser)->first();
+        $planzajec = Planzajec::where('GrupaStudenta_idGrupaStudenta', $student->grupastudenta->first()?->idGrupaStudenta)->first();
+        $zajecia = Zajecie::where('PlanZajec_idPlanZajec', $planzajec->idPlanZajec)->get();
+        $rezerwowanezajecia = Rezerwacja::where('Zajęcie_PlanZajec_idPlanZajec', $planzajec->idPlanZajec)->get();
+
+        $gotowyplan = $zajecia->concat($rezerwowanezajecia)->sortBy('GodzinaRozpoczecia');
+        dd($gotowyplan->toArray());
+
+        return Inertia::render('PlanZajec', [
+            'paln' => $gotowyplan->values()->toArray(),
+        ]);
     }
 
     public function mojedane(Request $request){
--- resources/js/Pages/PlanZajec.jsx
+++ resources/js/Pages/PlanZajec.jsx
@@ -13,7 +13,13 @@ function SiatkaPlanu(){
 function Content(){
 
     const godziny = [
-        '08:'
+        '08:00-09:00', '09:00-10:00', '10:00-11:00', '11:00-12:00',
+        '12:00-13:00', '13:00-14:00', '14:00-15:00', '15:00-16:00',
+        '16:00-17:00', '17:00-18:00', '18:00-19:00', '19:00-20:00', '20:00-21:00', 
+    ]
+
+    const dni = [
+        'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek'
     ]
 
     return(
@@ -25,9 +31,37 @@ function Content(){
                 <span className="font-bold text-[16px] text-white w-full">Plan zajęć</span>
             </div>
 
-            <div className="bg-[#08083b] w-full h-full flex border-[0.3px] border-white py-5 px-3 rounded-b-[10px] ">
+            <div className="bg-[#08083b] w-full h-[900px] flex border-[0.3px] border-white py-3 px-1 rounded-b-[10px] ">
                 <div className="w-[80%] border border-white rounded-l-[10px]">
-                    1
+                    
+                    <div className="grid grid-cols-6 border h-full border-slate-700/80 rounded-xl overflow-hidden bg-[#1a1738]">
+          
+                        <div className="bg-[#73768c] p-3 text-white text-center font-semibold border-b border-r border-white">
+                            Dzień
+                        </div>
+                        {dni.map((day) => (
+                            <div key={day} className="bg-[#73768c] p-3 text-center text-white font-semibold border-b border-r border-white last:border-r-0">
+                                {day}
+                            </div>
+                        ))}
+                        {godziny.map((hour) => (
+                            <React.Fragment key={hour}>
+                                <div className="h-full text-xs text-white border-b border-r border-white flex items-center justify-center bg-[#73768c]">
+                                    {hour}
+                                </div>
+                                {dni.map((day, dayIndex) => (
+                                    <div key={dayIndex} className="h-full border-b border-r border-white bg-[#676c98] relative last:border-r-0  transition-colors hover:bg-white/[0.02]">
+                                        {/* {hour === '10:00-11:00' && dayIndex === 2 && (
+                                            <div className="absolute inset-1 bg-purple-600 rounded-lg p-2 text-xs flex flex-col justify-between shadow-md">
+                                                <span className="font-bold leading-tight">Programowanie obiektowe w C++</span>
+                                                <span className="text-[10px] text-purple-200">dr inż. Jan Kowalski | E301</span>
+                                            </div>
+                                        )} */}
+                                    </div>
+                                ))}
+                            </React.Fragment>
+                        ))}
+                    </div>
                 </div>
                 <div className="w-[20%] rounded-r-[10px] bg-[#06062c] px-[20px] py-[33px] ">
                     
